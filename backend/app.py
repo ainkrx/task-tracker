9bf0da0b06f58757047cd371f00fd7f3c6a1c0fa
ainkrx <ainkrx@gmail.com>
2026-07-08 14:08:07 +0700

    switch to neon postgres

--- backend/app.py
+++ backend/app.py
@@ -11,13 +11,17 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = "sqlite:///./tasks.db"
-engine = create_engine(
-    DATABASE_URL,
-    # https://fastapi.tiangolo.com/tutorial/sql-databases/#create-an-engine
-    # 1 request session per thread
-    connect_args={"check_same_thread": False}
-)
+# SQLite
+# DATABASE_URL = "sqlite:///./tasks.db"
+# engine = create_engine(
+#     DATABASE_URL,
+#     # https://fastapi.tiangolo.com/tutorial/sql-databases/#create-an-engine
+#     # 1 request session per thread
+#     connect_args={"check_same_thread": False}
+# )
+# neon
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
